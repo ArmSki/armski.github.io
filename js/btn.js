1dc7091dd3380eec6e9410bde2bdbc3b93c3fc9b
ArmSki <ArmSki@users.noreply.github.com>
2018-01-16 00:53:17 +0400

    btn fix

--- js/btn.js
+++ js/btn.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
@@ -11,14 +21,12 @@ var  nav = document.getElementById('nav');
 
  window.onscroll = function(){
 
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   if (window.pageYOffset >3) {
 
-     document.getElementById("myBtn").style.display = "block"
      nav.style.background = "linear-gradient(to bottom,#1a6695,#3377a2)";
      nav.style.boxShadow = "0px 0px 0px blue";
    }
    else{
-     document.getElementById("myBtn").style.display = "none";
      nav.style.background = "transparent";
      nav.style.boxShadow = "none";
    }
