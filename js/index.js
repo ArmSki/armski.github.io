92f762f8ade95dae4569423ba229bfd6eff91d43
ArmSki <ArmSki@users.noreply.github.com>
2018-01-18 21:06:06 +0400

    nav bar background animation

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
+
+//@ window.onscroll ֆունկցիան կկանչի scrollFunction(), իսկ թե էդ ինչ կանի, նայի իրա մեջ
+var  nav = document.getElementById('nav');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+        document.getElementById("myBtn").style.display = "block";
+        nav.classList.add("Anime");
+        nav.classList.remove("Black");
+        console.log(window.pageYOffset);
+
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+        nav.classList.remove("Anime");
+        nav.classList.add("Black");
+      }
+}
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-// Scrolling --> nav bar background color change
-
-var  nav = document.getElementById('nav');
-
- window.onscroll = function(){
-
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-
-     document.getElementById("myBtn").style.display = "block"
-     nav.style.background = "linear-gradient(to bottom,#1a6695,#3377a2)";
-     nav.style.boxShadow = "0px 0px 0px blue";
-   }
-   else{
-     document.getElementById("myBtn").style.display = "none";
-     nav.style.background = "transparent";
-     nav.style.boxShadow = "none";
-   }
- }
